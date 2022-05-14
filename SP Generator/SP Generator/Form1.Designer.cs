ed4e6684cdf75a92296335dc08b66b5c69709c44
Jay Sedani <43343732+sedanijay@users.noreply.github.com>
2022-05-15 03:14:45 +0530

    Initial Commit
    -- Initial Commit

---
+++ SP Generator/SP Generator/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace SP_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_connectDB = new System.Windows.Forms.Button();
+            this.TB_connection_str1 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn_generateSP = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_connectDB
+            // 
+            this.btn_connectDB.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_connectDB.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_connectDB.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btn_connectDB.Location = new System.Drawing.Point(656, 70);
+            this.btn_connectDB.Name = "btn_connectDB";
+            this.btn_connectDB.Size = new System.Drawing.Size(131, 32);
+            this.btn_connectDB.TabIndex = 0;
+            this.btn_connectDB.Text = "Connect";
+            this.btn_connectDB.UseVisualStyleBackColor = false;
+            this.btn_connectDB.Click += new System.EventHandler(this.btn_connectDB_Click);
+            // 
+            // TB_connection_str1
+            // 
+            this.TB_connection_str1.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TB_connection_str1.Location = new System.Drawing.Point(29, 72);
+            this.TB_connection_str1.Name = "TB_connection_str1";
+            this.TB_connection_str1.Size = new System.Drawing.Size(615, 30);
+            this.TB_connection_str1.TabIndex = 1;
+            this.TB_connection_str1.Text = "<database connection string>";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(29, 117);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(427, 325);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // btn_generateSP
+            // 
+            this.btn_generateSP.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_generateSP.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_generateSP.Location = new System.Drawing.Point(462, 237);
+            this.btn_generateSP.Name = "btn_generateSP";
+            this.btn_generateSP.Size = new System.Drawing.Size(325, 41);
+            this.btn_generateSP.TabIndex = 3;
+            this.btn_generateSP.Text = "Generate SPs";
+            this.btn_generateSP.UseVisualStyleBackColor = false;
+            this.btn_generateSP.Visible = false;
+            this.btn_generateSP.Click += new System.EventHandler(this.btn_generateSP_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("MV Boli", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(787, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(32, 31);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(28, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(345, 35);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Stored Procedure Generator";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(823, 42);
+            this.panel1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(822, 509);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_generateSP);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.TB_connection_str1);
+            this.Controls.Add(this.btn_connectDB);
+            this.Font = new System.Drawing.Font("Nirmala UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_connectDB;
+        private System.Windows.Forms.TextBox TB_connection_str1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btn_generateSP;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ SP Generator/SP Generator/Form1.cs
@@ -0,0 +1,145 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SP_Generator
+{
+    public partial class Form1 : Form
+    {
+        private SP_utils obj_utils = new SP_utils();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private List<string> tables = new List<string>();
+
+
+        private void CheckBox_Checked(object sender, EventArgs e)
+        {
+            CheckBox chk = (sender as CheckBox);
+            chk.Enabled = false;
+            if (chk.Checked)
+            {
+                tables.Add(chk.Text);
+                //MessageBox.Show("You selected: " + chk.Text);
+            }
+        }
+
+        
+
+        private void btn_generateSP_Click(object sender, EventArgs e)
+        {
+            string str = "";
+            string conn = TB_connection_str1.Text.ToString();
+
+            foreach (var table_name in tables)
+            {
+                dynamic json_table_columns = obj_utils.FetchTableColumns(conn, table_name);
+
+                dynamic column_props = JsonConvert.DeserializeObject<dynamic>(json_table_columns);
+
+                string primary_key = obj_utils.GetPrimaryKey(conn, table_name);
+
+                string para_list = "@IU_Flag char \n,";
+                string col_list = "";
+                string val_list = "";
+                string updateParas = "";
+
+                foreach (var column_prop in column_props)
+                {
+                    para_list += "@" + column_prop["name"] + " " + column_prop["type"] + ",\n";
+
+                    if (column_prop["name"]!= primary_key && column_prop["type"] != "timestamp" && column_prop["type"] != "time" && column_prop["type"] != "smalldatetime")
+                    {
+                        col_list += column_prop["name"] + ",";
+                        val_list += "@" + column_prop["name"] + ",";
+                        updateParas += column_prop["name"] + " = @" + column_prop["name"] + ",";
+                    }
+                }
+
+                para_list = para_list.TrimEnd('\n');
+                para_list = para_list.TrimEnd(',');
+                col_list = col_list.TrimEnd(',');
+                val_list = val_list.TrimEnd(',');
+                updateParas = updateParas.TrimEnd(',');
+
+
+                obj_utils.prepare_SP(primary_key, para_list, col_list,val_list, updateParas, table_name);
+
+
+                //str += table_name + ",";
+
+
+            }
+
+            //MessageBox.Show("Selected Tables : " + str);
+            string project_path = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
+            MessageBox.Show("Procedures location : " + project_path + "/SP_Files", "Message");
+        }
+
+        private void btn_connectDB_Click(object sender, EventArgs e)
+        {
+            string conn = TB_connection_str1.Text.ToString();
+            string cmd = "SELECT * FROM INFORMATION_SCHEMA.TABLES";
+
+            try
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter(cmd, conn))
+                {
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+
+                    //Loop and add CheckBoxes to FloyLayoutPanel.
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        CheckBox chk = new CheckBox();
+                        chk.Width = 200;
+                        chk.Text = row["TABLE_NAME"].ToString();
+                        chk.CheckedChanged += new EventHandler(CheckBox_Checked);
+                        flowLayoutPanel1.Controls.Add(chk);
+                    }
+
+                }
+                btn_generateSP.Visible = true;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(),"Connecting Error Message");
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (System.Windows.Forms.Application.MessageLoop)
+            {
+                // WinForms app
+                System.Windows.Forms.Application.Exit();
+            }
+            else
+            {
+                // Console app
+                System.Environment.Exit(1);
+            }
+        }
+
+    }
+}
